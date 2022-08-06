d5d0de83475f082bd36bdee199967ea9469e7d11
Your Name <you@example.com>
2022-08-06 01:00:05 -1200

    updated-version

--- contracts/PBANK.sol
+++ contracts/PBANK.sol
@@ -1,24 +1,40 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
 
-contract PBANK{
-    event deposit(uint amount);
-    event withdraw(uint amount);
+contract Pbank{
+    event Deposit(address caller, uint amount);
+    event withdraw(address caller,uint amount);
 
-    address public owner;
+    // address public owner;
 
-    modifier onlyowner(){
-        require(msg.sender == owner, "not the owner");
-        _;
+    struct Users{
+        address  owner;
+        uint userBalance;
+        bool hasWithdrawn;
+
     }
+    mapping(address => Users) public user;
 
-    receive() external payable{
-        emit deposit(msg.value);
-    }
+    function deposit() payable external {
+        Users storage user1 = user[msg.sender];
+        // require( msg.sender == user1.owner, "not the owner");
+        user1.owner = msg.sender;
+        user1.userBalance = user1.userBalance + msg.value;
+        user1.hasWithdrawn = false;
+        emit Deposit(msg.sender, msg.value);
+        
+    } 
 
-    function Withdraw() external onlyowner {
-        emit withdraw(address(this).balance);
-        selfdestruct(payable(msg.sender));
+    function Withdraw(uint _amount) external  {
+        Users storage user1 = user[msg.sender];
+        require(_amount == user1.userBalance, "invalid amount");
+        require(user1.hasWithdrawn != true, "cant withdraw again");
+        require( msg.sender == user1.owner, "not the owner");
+        user1.userBalance = user1.userBalance - _amount;
+        user1.hasWithdrawn = true;
+        payable(user1.owner).transfer(_amount);
+        emit withdraw(user1.owner , _amount);
+        selfdestruct(payable(user1.owner));
     }
 
 }
